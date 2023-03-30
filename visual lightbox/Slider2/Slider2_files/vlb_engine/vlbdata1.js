8d9667b47a3e525cb3a484513009b696c04513c3
Varun0311 <84927648+Varun0311@users.noreply.github.com>
2023-03-30 15:14:51 +0530

    Delete visual lightbox directory

--- visual lightbox/Slider2/Slider2_files/vlb_engine/vlbdata1.js
+++
@@ -1,1 +0,0 @@
-jQuery(document).ready(function(){ window.Lightbox = new jQuery().visualLightbox({autoPlay:false,borderSize:10,classNames:'vlightbox1',closeLocation:'top',descSliding:true,enableRightClick:true,enableSlideshow:true,floating:false,openByHover:true,prefix:'vlb1',resizeSpeed:6,slideTime:6,startZoom:true}) });
