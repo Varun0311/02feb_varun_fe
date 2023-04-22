ece84a3681379fff6491c19ccf2ca222520045b4
Varun0311 <84927648+Varun0311@users.noreply.github.com>
2023-04-22 09:40:40 +0530

    Delete task directory

--- task/Template2.js
+++
@@ -1,9 +0,0 @@
-
-$(document).ready(function(){
-    // $('#slider').cycle('fade');
-    // $('#slider').cycle('zoom');
-    // $('#slider').cycle('scrollRight');
-    $('.sld-img').cycle('scrollLeft');
-    // $('#slider').cycle('shuffle');
-    // $('#slider').cycle('zoom, fade, scrollRight');
-})
